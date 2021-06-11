df83dc67a69f05fe9e52ad8cfc15325e14e0abf5
Tito <ebenirot@gmail.com>
2021-06-11 13:20:05 +0100

    --- Auto-Git Commit ---

--- src/models/user.js
+++ src/models/user.js
@@ -1,3 +1,28 @@
 const mongoose = require("mongoose")
 
-const userSchema = new mongoose.Schema()
+const userSchema = new mongoose.Schema({
+    firstName: {
+        type: String,
+        required: true
+    },
+    lastName: {
+        type: String,
+        required: true
+    },
+    userName: {
+        type: String,
+        required: true,
+        unique: true,
+        lowerCase: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+})
+
+mongoose.set('useCreateIndex', true);
+
+const User = mongoose.model("user", userSchema);
+
+module.exports = User;
