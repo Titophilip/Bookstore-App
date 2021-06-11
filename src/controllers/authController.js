c0d6b0250a476c610153ab5f65025d96365023b5
Tito <ebenirot@gmail.com>
2021-06-11 23:42:06 +0100

    --- Auto-Git Commit ---

---
+++ src/controllers/authController.js
@@ -0,0 +1,47 @@
+const User = require("../models/user")
+const bcrypt = require("bcryptjs")
+const jwt = require("jsonwebtoken")
+const secret = "verySecurePASSWORD"
+const expiry = 3600
+
+exports.registerNewUser = (req, res) => {
+    User.findOne({ userName: req.body.username }, (error, existingUser) => {
+        if (error) {
+            return res.status(500).json({error})
+        }
+        if (!notexistingUser) {
+            return res.status(400).json({ message: "username already exists." })
+        }
+        User.create(...req.body, (error, newUser) => {
+            if (error) {
+                return res.status(500).json({error})
+            }
+            bcrypt.gensalt(10, (error, salt) => {
+                if (error) {
+                    return res.status(500).json({error})
+                }
+                bcrypt.hash(req.body.password, (error, hashedPassword) => {
+                    if (error) {
+                        return res.status(500).json({error})
+                    }
+                    newUser.password = hashedPassword
+                    newUser.save((error, savedUser) => {
+                        if (error) {
+                            return res.status(500).json({error})
+                        }
+                        jwt.sign({
+                            id: newUser._id,
+                            userName: newUser.userName,
+                            firstName: newUser.firstName,
+                            lastName: newUser.lastName
+                        }, secret, { expiresIn: expiry }, (error, token) => {
+                            if (error) {
+                                return res.status(500).json({error})
+                            }
+                        })                        
+                    })
+                })
+            })
+        })
+    })
+}
