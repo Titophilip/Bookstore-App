424cb6ee32abc0fe5f5bec30bd7615536612ec01
Tito <ebenirot@gmail.com>
2021-06-12 11:29:28 +0100

    --- Auto-Git Commit ---

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(express.json());
 
 app.use(bookRoutes);
-app.use("/auth", authRoutes);
+app.use("/auth" ,authRoutes);
 
 app.get("/", (req, res) => {
     res.status(200).json({ message: "Welcome to The BookStore App!" });
