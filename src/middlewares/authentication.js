481e378f8f8a93dae0290a506bc418fb6f590bf6
Tito <ebenirot@gmail.com>
2021-06-14 12:14:15 +0100

    --- Auto-Git Commit ---

--- src/middlewares/authentication.js
+++ src/middlewares/authentication.js
@@ -3,5 +3,9 @@ exports.authenticateUser =(req, res, next) => {
         return res.status(401).json({ message: "Authentication header required."})
     }
     let splittedHeader = req.headers.authorization.split(" ")
+    if (splittedHeader[0] !== "Bearer") {
+        return res.status(401).json({ message: "Authorization format is Bearer <token>"})
+    }
     
+    next()
 }
