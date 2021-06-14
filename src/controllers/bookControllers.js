1a34f82e5b8770863363da20f4f2c14b2ec9b03d
Tito <ebenirot@gmail.com>
2021-06-14 11:33:40 +0100

    --- Auto-Git Commit ---

--- src/controllers/bookControllers.js
+++ src/controllers/bookControllers.js
@@ -1,7 +1,7 @@
 const Book = require("../models/book.js");
 
 exports.createNewBook = (req, res) => {
-    Book.create(...req.body, (error, newBook) => {
+    Book.create(req.body, (error, newBook) => {
         if (error) {
             return res.status(500).json({ message: error });
         } else {
