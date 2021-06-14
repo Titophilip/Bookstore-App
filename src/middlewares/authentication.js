a44cfa92e58e74d998eff50bfd37a0025a2d3f1a
Tito <ebenirot@gmail.com>
2021-06-14 11:53:56 +0100

    --- Auto-Git Commit ---

--- src/middlewares/authentication.js
+++ src/middlewares/authentication.js
@@ -1,3 +1,6 @@
 exports.authenticateUser =(req, res, next) => {
-    
+    if (!req.headers.authorization) {
+        return res.status(401).json({ message: "Authentication header required."})
+    }
+    next()
 }
