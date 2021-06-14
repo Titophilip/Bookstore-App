5d81cdff6a90b92d30c289588d9b223aadbc4264
Tito <ebenirot@gmail.com>
2021-06-14 12:24:29 +0100

    --- Auto-Git Commit ---

--- src/middlewares/authentication.js
+++ src/middlewares/authentication.js
@@ -1,11 +1,25 @@
+const jwt = require("jsonwebtoken")
+const secret = process.env.SECRET
+
 exports.authenticateUser =(req, res, next) => {
     if (!req.headers.authorization) {
         return res.status(401).json({ message: "Authentication header required."})
     }
-    let splittedHeader = req.headers.authorization.split(" ")
+    let splittedHeader = req.headers.authorization.split(" ");
+
     if (splittedHeader[0] !== "Bearer") {
         return res.status(401).json({ message: "Authorization format is Bearer <token>"})
     }
-    
+    let token = splittedHeader[1];
+
+    jwt.verify(token, secret, (error, decodedToken) => {
+        if (error) {
+            return res.status(500).json({ error })
+        }
+        if (!decodedToken) {
+            return res.status(401).json({ message: "Invalid authorization token" })
+        }
+    })
+
     next()
 }
